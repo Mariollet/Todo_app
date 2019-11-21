43337dfb928069bf159a5368aaf86e68bbdf97c8
Azzerty23 <azzerty23@hotmail.fr>
2019-11-21 13:19:54 +0100

    add email#create controller

--- app/controllers/email_controller.rb
+++ app/controllers/email_controller.rb
@@ -4,4 +4,21 @@ class EmailController < ApplicationController
     @emails = Email.all
   end
 
+  def create
+    @email = Email.new(
+      object: Faker::Cannabis.health_benefit,
+      body: Faker::Lorem.paragraph(6),
+   )
+    if @email.save
+      flash[:notice] = "Email created"
+      respond_to do |format|
+        format.html { redirect_to root_path }
+        format.js { }
+      end
+    else
+      redirect_to root_path
+      flash[:notice] = "Please try again"
+    end
+  end
+
 end
